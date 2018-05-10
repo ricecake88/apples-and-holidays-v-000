a3cff48ba09e95dea1c29c438840025b8dc1069e
Grace Shih <vocalise@gmail.com>
2018-05-10 02:51:19 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,10 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+   holiday_hash.each do |season, holiday|
+     holiday.values.flatten
+   end
+   holiday
 end
 
 def all_supplies_in_holidays(holiday_hash)
